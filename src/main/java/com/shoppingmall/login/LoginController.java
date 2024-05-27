57cdd545ce83b5607657b14914214db25a510720
ohsoomansour <ceoosm@naver.com>
2024-05-27 09:42:23 +0900

    "5.27 setting"

--- src/main/java/com/shoppingmall/login/LoginController.java
+++ src/main/java/com/shoppingmall/login/LoginController.java
@@ -1,10 +1,18 @@
 package com.shoppingmall.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	private LoginService loginService;
 	
 	
+	/*
+	 *@Author
+	 *
+	 *
+	 * */ 
 }
